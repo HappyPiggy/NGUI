a0f7c94cde0a803a2af259b3928b8aa849a96359
Pigggger <296056428@qq.com>
2016-10-17 23:10:33 +0800

    add chatSystem UI

---
+++ NGUI_Demos/Assets/submit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class submit : MonoBehaviour {
+
+    private UIInput uiInput;
+    public UITextList textList;
+
+    void Awake()
+    {
+        uiInput = GetComponent<UIInput>();
+    }
+
+
+   public void OnSubmitClick()
+    {
+        string submit = uiInput.value;
+        textList.Add(submit);
+        uiInput.value = "";
+    }
+}
