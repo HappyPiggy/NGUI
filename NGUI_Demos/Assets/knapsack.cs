d64829d843950c1ae5842956b93962f570cf7158
Pigggger <296056428@qq.com>
2016-10-18 16:20:20 +0800

    add knapsack ui

---
+++ NGUI_Demos/Assets/knapsack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class knapsack :UIDragDropItem {
+
+    protected override void OnDragDropRelease(GameObject surface)
+    {
+        base.OnDragDropRelease(surface);
+        print(surface.tag);
+
+        if (surface.tag == "Cell")
+        {
+            transform.parent = surface.transform;
+            transform.localPosition = Vector3.zero;
+        }
+        else if (surface.tag == "Goods")
+        {
+            Transform parent = transform.parent;
+            transform.parent = surface.transform.parent;
+            transform.localPosition = Vector3.zero;
+
+            surface.transform.parent = parent;
+            surface.transform.localPosition = Vector3.zero;
+        }
+    }
+
+}
