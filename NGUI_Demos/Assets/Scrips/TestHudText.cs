44f43952800c14bf703c9f781445a2460c86e177
Pigggger <296056428@qq.com>
2016-10-30 22:15:24 +0800

    add hudtext  plugin

---
+++ NGUI_Demos/Assets/Scrips/TestHudText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestHudText : MonoBehaviour {
+
+
+    public HUDText text;
+
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            text.Add("-10", Color.red, 1);
+        }
+    }
+}
